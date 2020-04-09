59dcd6b495ed5ae277fc15a2856f6036d69f6d64
62668607 <asia.ryfka@gmail.com>
2020-04-09 20:46:52 +0200

    Step 9: Formatting Values

--- webapp/index.js
+++ webapp/index.js
@@ -7,7 +7,6 @@ sap.ui.require([
 
 	// Attach an anonymous function to the SAPUI5 'init' event
 	sap.ui.getCore().attachInit(function () {
-		// Create a JSON model from an object literal
 		var oModel = new JSONModel({
 			firstName: "Harry",
 			lastName: "Hawk",
@@ -18,24 +17,20 @@ sap.ui.require([
 				zip: "69190",
 				country: "Germany"
 			}
-
 		});
 
 		// Assign the model object to the SAPUI5 core
 		sap.ui.getCore().setModel(oModel);
 
-		// Create a resource bundle for language specific texts
-		var oResourceModel = new ResourceModel({
+		var oResourceBundle = new ResourceModel({
 			bundleName: "sap.ui.demo.db.i18n.i18n"
 		});
 
-		// Assign the model object to the SAPUI5 core using the name "i18n"
-		sap.ui.getCore().setModel(oResourceModel, "i18n");
+		sap.ui.getCore().setModel(oResourceBundle, "i18n");
 
 		// Display the XML view called "App"
 		new XMLView({
 			viewName: "sap.ui.demo.db.view.App"
 		}).placeAt("content");
-
 	});
 });
